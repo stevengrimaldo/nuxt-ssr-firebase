bb092c9c70a140b238ad33341bd27e0fa774cbfe
Steven Grimaldo <sgrimaldo@gsandf.com>
2019-05-17 16:41:08 -0500

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="mui-appbar mui--z1">
+    <nav>
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/page2">Page 2</nuxt-link>
+      <nuxt-link to="/page3">Page 3</nuxt-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "header"
+};
+</script>
+
+<style lang="css">
+header {
+  box-sizing: border-box;
+  background-color: #eee;
+  border-top: 1px solid #e0e0e0;
+  padding: 0.5em 0;
+  display: flex;
+  align-items: center;
+}
+
+.header-flex-item {
+  margin: 0.5em;
+}
+</style>
---
+++ src/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app-wrapper">
+    <AppHeader/>
+    <div id="content-wrapper" class="mui--text-center">
+      <nuxt class="mui-container"/>
+    </div>
+    <AppFooter/>
+  </div>
+</template>
+
+<script>
+import AppFooter from "~/components/Footer";
+import AppHeader from "~/components/Header";
+
+export default {
+  components: { AppHeader, AppFooter },
+  methods: {
+    reloadPage() {
+      window.location.reload();
+    }
+  }
+};
+</script>
+
+<style>
+#app-wrapper {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+</style>
